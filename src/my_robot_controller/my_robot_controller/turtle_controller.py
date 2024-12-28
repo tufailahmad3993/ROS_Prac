9ede1ae39e800ed994cb7bea9f1513fac5f2cd8d
tufailahmad3993 <tufailahmad695@gmail.com>
2024-12-28 15:35:02 +0900

    adding closed loop node

--- src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -1,10 +1,29 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+
 
 class TurtleControllerNode(Node):
     def __init__(self):
-        super().__init__('turtlecontroller')
-        self.get_logger().info("Turtle controller has been started")
+        super().__init__('turtle_controller')
+        self.cmd_vel_pub_ = self.create_publisher(Twist, '/turtle1/cmd_vel' , 10)
+        self.get_logger().info("turtle controller has been started")
+        self.pose_subscriber_ = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10 )
+
+    def pose_callback(self, msg):
+        cmd = Twist()
+        msg = Pose()
+
+        if msg.x > 9.0 or msg.x < 2.0 or msg.y > 9.0 or msg.y < 2.0 :
+            cmd.linear.x = 1.0   
+            cmd.angular.z = 0.1
+
+        else:
+            cmd.linear.x = 5.0
+            cmd.angular.z = 0.0
+        self.cmd_vel_pub_.publish(cmd)
 
 
 def main(args =None):
