308178686f5820eda9cd485c216e07121f56fdc8
tufailahmad3993 <tufailahmad695@gmail.com>
2025-01-01 16:12:36 +0900

    update action client

--- src/actions_py/actions_py/count_until_client.py
+++ src/actions_py/actions_py/count_until_client.py
@@ -1,8 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from my_robot_interfaces.action import CountUntil
+from rclpy.action.client import ClientGoalHandle
 from rclpy.action import ActionClient
-import time
 
 
 class CountUntilClientNode(Node):
@@ -26,12 +26,17 @@ class CountUntilClientNode(Node):
 
        # Send the goal
        self.get_logger().info("Sending Goal")
-       self.count_until_client_.send_goal_async(goal)
-       
+       self.count_until_client_.send_goal_async(goal).add_done_callback(self.goal_response_callback)
 
+    def goal_response_callback(self, future):
+        self.goal_handle_ : ClientGoalHandle = future.result()
 
+        if self.goal_handle_.accepted:
+            self.goal_handle_.get_result_async().add_done_callback(self.goal_result_callback)
 
-
+    def goal_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info("Result: " + str(result.reached_number))
 
 
 def main(args = None):
--- src/actions_py/setup.py
+++ src/actions_py/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "count_until_server =  actions_py.count_until_server:main"
+            "count_until_server =  actions_py.count_until_server:main",
+            "count_until_client =  actions_py.count_until_client:main"
         ],
     },
 )
